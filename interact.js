1430bd25619fe129230a896f16ced1af830d8f57
SabaiaKochia <kochadzes@wit.edu>
2023-03-20 23:02:54 -0400

    Publish

--- interact.js
+++ interact.js
@@ -10,3 +10,11 @@
 // 3D render split into photos/images and then use apple's style of website to create an animated webpage front background
 
 //TEST
+const frameHeight = 102;
+const frames = 15;
+const div = document.getElementById("animation");
+let frame = 0;
+setInterval(function () {
+    const frameOffset = (++frame % frames) * -frameHeight;
+    div.style.backgroundPosition = "0px " + frameOffset + "px";
+}, 100);
